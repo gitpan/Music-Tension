599b6b67b2d8ebf50c7fe1f74c1f43fca90eff34
Jeremy Mates <jmates@cpan.org>
2012-11-11 16:17:42 +0000

    Import of JMATES/Music-Tension-0.50 from CPAN.
    gitpan-cpan-distribution: Music-Tension
    gitpan-cpan-version:      0.50
    gitpan-cpan-path:         JMATES/Music-Tension-0.50.tar.gz
    gitpan-cpan-author:       JMATES
    gitpan-cpan-maturity:     released

--- lib/Music/Tension/Cope.pm
+++ lib/Music/Tension/Cope.pm
@@ -1,6 +1,6 @@
 # "Copian" tension analysis for 12-pitch material in equal temperament.
 #
-# Beta interface! May be subject to change without notice!
+# Beta interface! May change without notice!
 
 package Music::Tension::Cope;
 
@@ -9,11 +9,10 @@ use strict;
 use warnings;
 
 use Carp qw/croak/;
-use List::Util qw/max min sum/;
 use Scalar::Util qw/looks_like_number/;
 
 our @ISA     = qw(Music::Tension);    # but doesn't do anything right now
-our $VERSION = '0.01';
+our $VERSION = '0.50';
 
 my $DEG_IN_SCALE = 12;
 
@@ -21,13 +20,40 @@ my $DEG_IN_SCALE = 12;
 #
 # SUBROUTINES
 
-# boilerplate until figure out something better
 sub new {
   my ( $class, %param ) = @_;
   my $self = {};
 
+  if ( exists $param{duration_weight} ) {
+    croak "duration_weight must be a number"
+      if !looks_like_number $param{duration_weight};
+    $self->{_duration_weight} = $param{duration_weight};
+  } else {
+    $self->{_duration_weight} = 0.1;
+  }
+
+  if ( exists $param{metric_weight} ) {
+    croak "metric_weight must be a number"
+      if !looks_like_number $param{metric_weight};
+    $self->{_metric_weight} = $param{metric_weight};
+  } else {
+    $self->{_metric_weight} = 0.1;
+  }
+
+  if ( exists $param{octave_adjust} ) {
+    croak "octave_adjust must be a number"
+      if !looks_like_number $param{octave_adjust};
+    $self->{_octave_adjust} = $param{octave_adjust};
+  } else {
+    $self->{_octave_adjust} = -0.02;
+  }
+
   if ( exists $param{tensions} ) {
     croak "tensions must be hash reference" if ref $param{tensions} ne 'HASH';
+    for my $i ( 0 .. 11 ) {
+      croak "tensions must include all intervals from 0 through 11"
+        if !exists $param{tensions}->{$i};
+    }
     $self->{_tensions} = $param{tensions};
   } else {
     # Default interval tentions taken from "Computer Models of Musical
@@ -49,52 +75,87 @@ sub new {
     };
   }
 
-  if ( exists $param{octave_adjust} ) {
-    croak "octave adjust must be a number"
-      if !looks_like_number $param{octave_adjust};
-    $self->{_octave_adjust} = $param{octave_adjust};
-  } else {
-    $self->{_octave_adjust} = -0.02;
-  }
-
   bless $self, $class;
   return $self;
 }
 
-# TODO additional routines to consider rhythmic position/phrase
-# considerations (e.g. highest or lowest tension for a series of notes,
-# assuming a cadence, and "good enough" weighting if on or off The Beat.
+# Approach tension - horizontal tension, I'm assuming harmonic function,
+# therefore limit to intervals in same register.
+sub approach {
+  my ( $self, $p1 ) = @_;
+  croak "pitch is required" if !defined $p1;
+  croak "pitch must be integer" if $p1 !~ m/^-?\d+$/;
 
-# Tension from each lowest note to all others above it in a passed pitch
-# set. Returns average, max, min, array ref of tensions. TODO might need
-# a better name.
-sub pcs {
-  my ( $self, $pset ) = @_;
-  croak "pitch set must be array ref\n" unless ref $pset eq 'ARRAY';
-  croak "pitch set must contain something\n" if !@$pset;
-  my @pcs = @$pset;
-
-  # Reposition pitches upwards if subsequent lower than initial notes
-  # (makes inversions more dissonant than root position chords, for one,
-  # and does the "right thing" with <c e g c> or qw/0 4 7 0/).
-  for my $i ( 1 .. $#pcs ) {
-    while ( $pcs[$i] < $pcs[ $i - 1 ] ) {
-      $pcs[$i] += $DEG_IN_SCALE;
-    }
-  }
-
-  my @tensions;
-  for my $i ( 0 .. $#pcs - 1 ) {
-    for my $j ( $i + 1 .. $#pcs ) {
-      push @tensions, $self->pitches( $pcs[$i], $pcs[$j] );
-    }
-  }
-
-  return sum(@tensions) / @tensions, min(@tensions), max(@tensions),
-    \@tensions;
+  $self->pitches( 0, $p1 % $DEG_IN_SCALE );
 }
 
-# TODO might need better name
+# Tension over durations
+sub duration {
+  my ( $self, $input, $duration ) = @_;
+
+  croak "duration must be a positive value"
+    if !looks_like_number($duration)
+      or $duration <= 0;
+
+  my $tension;
+  if ( ref $input eq 'ARRAY' ) {
+    $tension = $self->vertical($input);
+  } elsif ( looks_like_number($input) ) {
+    $tension = $input;
+  } else {
+    croak "unknown pitch set or prior tension value '$input'";
+  }
+
+  # p.232-233 [Cope 2005] - this result "is then added to any grouping's
+  #   accumulated tension weighting"
+  return $self->{_duration_weight} * $duration +
+    $self->{_duration_weight} * $tension;
+}
+
+# Tension based on where note is within measure p.232 [Cope 2005]
+sub metric {
+  my ( $self, $b, $v ) = @_;
+  croak "input must be positive numeric"
+    if !looks_like_number($b)
+      or $b <= 0
+      or !looks_like_number($v)
+      or $v <= 0;
+
+  return ( $b * $self->{_metric_weight} ) / $v;
+}
+
+# Tension from first note to all others above it in a passed pitch set.
+# Returns sum, min, max, and array ref of tensions, unless just the sum
+# is desired by context.
+sub vertical {
+  my ( $self, $pset ) = @_;
+  croak "pitch set must be array ref\n" unless ref $pset eq 'ARRAY';
+  croak "pitch set must contain multiple elements\n" if @$pset < 2;
+  my @pcs = @$pset;
+
+  # Reposition pitches upwards if subsequent lower than the initial pitch
+  for my $i ( 1 .. $#pcs ) {
+    if ( $pcs[$i] < $pcs[0] ) {
+      $pcs[$i] += $DEG_IN_SCALE +
+        ( int( ( $pcs[0] - $pcs[$i] - 1 ) / $DEG_IN_SCALE ) ) * $DEG_IN_SCALE;
+    }
+  }
+
+  my $min = ~0;
+  my $max = 0;
+  my ( @tensions, $sum );
+  for my $j ( 1 .. $#pcs ) {
+    my $t = $self->pitches( $pcs[0], $pcs[$j] );
+    $sum += $t;
+    $min = $t if $t < $min;
+    $max = $t if $t > $max;
+    push @tensions, $t;
+  }
+
+  return wantarray ? ( $sum, $min, $max, \@tensions ) : $sum;
+}
+
+# Tension for two pitches
 sub pitches {
   my ( $self, $p1, $p2 ) = @_;
   croak "two pitches required" if !defined $p1 or !defined $p2;
@@ -117,38 +178,183 @@ __END__
 
 =head1 NAME
 
-Music::Tension::Cope - tension analysis for 12 pitch material
+Music::Tension::Cope - tension analysis for equal temperament music
 
 =head1 SYNOPSIS
 
-Beta interface! May be subject to change without notice!
+Beta interface! Has and will change without notice!
 
   use Music::Tension;
-  my $mtc = Music::Tension->new;
+  my $tension = Music::Tension->new;
 
-  my $tension = $mtc->pitches(4, 17);
-  my ( $t_avg, $t_min, $t_max, $t_ref ) = $mtc->pcs(qw/0 4 7/);
+  my $value = $tension->pitches(4, 17);
+
+  my $sum                     = $tension->vertical([qw/0 4 7/]);
+  my ($sum, $min, $max, $ref) = $tension->vertical([qw/0 4 7/]);
+
+  $tension->duration( $sum,        1/4 );
+  $tension->duration( [qw/0 4 7/], 1/8 );
+
+  $tension->metric(1, 2);  # beat 1, with custom value 2
+
+  $tension->approach(7);   # motion by perfect fifth from prev.
 
 =head1 DESCRIPTION
 
-Music tension (how consonant or dissonant the pitches are) analysis of
-equal temperament intervals, David Cope style. The two methods (besides
-B<new> for blessings) are B<pitches> which accepts two integer pitches
-and returns the tension for them, and B<pcs>. B<pcs> is more
-complicated, accepting an array reference of pitches, and performs
-automatic octave adjustment (the pitches are assumed to be from lowest
-note to highest; should a pitch later in the list be lower than the
-previous pitch, it will be bumped up as many octaves as necessary)
-before tallying the tension on each lowest note with all higher notes in
-turn. This means chords such as <c dis e g> will have the minor 2nd in
-the middle counted, instead of just from c upwards, and that inversions
-will be ranked with higher tension than the root position chord (I <c e
-g> vs. I6 <e g c> vs. I64 <g c e>).
+Parsing music into a form suitable for use by this module and practical
+uses of the results are left as an exercise to the reader.
 
-B<pcs> currently ranks the 2nd inversion of the major triad as less
-tense than a 1st inversion of said traid, so that might bear looking
-into (overtone analysis to find how off-root what looks like the root
-is? but that would only work for triads, not non-triadic pitch sets).
+This module offers tension analysis of equal temperament 12-pitch music,
+using the method outlined by David Cope in the text "Computer Models of
+Musical Creativity". The various methods will calculate the tension of
+verticals (simultaneous pitches), tension over a given duration, and so
+forth. Larger numbers indicate greater tension (dissonance) and smaller
+numbers consonance.
+
+Cope uses the sum of the methods B<approach>, B<duration>, B<metric>,
+and B<vertical> to calculate the overall tension for each beat in an
+example Chorale. B<approach> and B<metric> will be the trickiest to
+implement, as they rely on knowing the interval of the harmonic change
+between the beats or having a lookup table available to calculate
+tension for random beats in random time signatures.
+
+Various details are not captured by the tension analysis, notably if a
+particular pitch is chromatic (implying an underlying key that is
+being diverged from), musical style, dynamics, the sonic envelope, and
+so forth. If these are important, they should be included in the
+tension analysis.
+
+Tension results may change between releases due to code changes. Be sure
+to update all old tension values before starting any new analysis or
+composition. This may require storing the original intervals or pitch
+sets along with the tension numbers.
+
+=head1 METHODS
+
+Any method may B<croak> if something is awry with the input.
+
+=over 4
+
+=item B<new> I<optional params>
+
+Constructor. Accepts optional parameters that specify alternate values
+instead of using the Cope-derived defaults.
+
+  my $tension = Music::Tension::Cope->new(
+    duration_weight => 0.42,
+    metric_weight   => 0.42,
+    octave_adjust   => 0.42,
+    tensions        => { 0 => 0.42, 1 => 0.42, ... },
+  );
+
+=over 4
+
+=item *
+
+I<duration_weight> adjusts the weighting given to B<duration> tensions.
+
+=item *
+
+I<metric_weight> adjusts the weighting given to B<metric> tensions.
+
+=item *
+
+I<octave_adjust> is a number to adjust intervals greater than an octave
+by. Intervals a single or multiple registers above the root will receive
+the same adjustment.
+
+=item *
+
+I<tensions> must be a hash reference that must contain all intervals
+from (unison) to 11 (major seventh) inclusive. The default values are a
+distillation of an involved consideration of the overtone series by
+Cope; see the references below for the gory details.
+
+=back
+
+=item B<approach> I<pitch1>
+
+Presently a thin wrapper around B<pitches>, where I<pitch1> is relative
+to unison (0), and will be mapped to the same register. Used for
+horizontal tensions. Cope indicates this is for "root motions" which
+from the example provided appears to be the harmonic changes, not
+specific interval leaps, so tension of unison for a tonic extension,
+tension of fifth for I-V6-I stasis or trips up or down the circle of
+fifths, and so forth:
+
+  $tension->approach( 0 );    # stasis (tonic -> tonic)
+  $tension->approach( 5 );    # perfect fourth (tonic -> pre-dominant)
+  $tension->approach( 7 );    # fifth (tonic -> dominant)
+
+=item B<duration> I<pitch_set_or_tension>, I<duration>
+
+Calculates and returns the duration tension of a given pitch set
+reference or prior tension value for a given duration. The duration
+tension increases in proportion to the input tension and magnitude of
+the duration.
+
+The exact value of the duration parameter is largely irrelevant as long
+as shorter durations use smaller values, and that the durations used are
+consistent over an analysis or composition. It could be a value in
+seconds, or a fraction 1/16 for a 16th note and then 1 for a whole note,
+or whatever. If using notes, be sure to factor in tempo if there are
+significant alterations to that over the course of a work.
+
+The duration tension may also need adjustment depending on how well the
+instrument involved sustains; consider a xylophone vs. a piano vs. a
+piano with the sustain pedal down vs. a church organ.
+
+=item B<metric> I<beat_number>, I<beat_value>
+
+Tension calculation based on the position in a measure. The beat number
+should be an positive integer (1 for first beat of measure, 2 for
+second, etc) and the value a non-zero number used to adjust the results.
+
+Cope indicates the use of a lookup table to provide the value, due to
+the complexity of where the weightings occur depending on the meter
+(e.g. 3/4 stresses the first (and perhaps second) beats, while 6/8 has
+stress on first and fourth). Lilypond auto-beaming should show one
+opinion on how notes are grouped and therefore where the stresses are,
+among other sources. Cope's tension values are lower on the beat, and
+higher towards the end of the measure:
+
+            4/4 time
+  ----------------------------------
+  beat     | 1     2     3     4
+  value    | 2     2     6     2
+  tension  | 0.05  0.10  0.05  0.20
+
+=item B<vertical> I<pitch_set_reference>
+
+B<vertical> accepts an array reference of pitches (integers), and
+tallies tensions between the initial pitch to each subsequent.
+B<vertical> will move subsequent pitches up a register if they are below
+the first pitch:
+
+  <10 0 4 7> is considered as <10 12 16 19>
+
+Unisons with the initial pitch will not be adjusted upwards. Octaves
+below the initial pitch will be adjusted to unison. If the
+adjustments are a problem, ensure that the first pitch is the lowest
+of the pitch set.
+
+B<vertical> returns the tension, minimum tension, maximum tension, and a
+reference to a list of tensions for each interval. Except in scalar
+context, where just the tension value is returned.
+
+An alternative method would be to perform tension checks on each pitch
+to any higher pitches, such that C<0 3 4 5> would also count the
+intervals present above the root (3 to 4, 3 to 5, and 4 to 5), instead
+of just the minor 3rd, major 3rd, and perfect fourth up from the root.
+An earlier version of this module did so, but the current code is trying
+to follow what Cope does as closely as possible.
+
+=item B<pitches> I<pitch1>, I<pitch2>
+
+Accepts two pitches (integers) and returns the tension of the interval
+formed between those two pitches.
+
+=back
 
 =head1 SEE ALSO
 
@@ -156,21 +362,19 @@ is? but that would only work for triads, not non-triadic pitch sets).
 
 =item *
 
-"Computer Models of Musical Creativity", David Cope, 2005. ISBN
-0-262-03338-0. Tension values shamelessly lifted from this book.
+L<App::MusicTools>
 
 =item *
 
-Music::AtonalUtil
+"Computer Models of Musical Creativity", David Cope, 2005, p.229-235.
 
 =item *
 
-Music::Chord::Positions
+"The Craft of Musical Composition", Paul Hindemith, 1942. (4th edition)
 
 =item *
 
-"Theory of Harmony", Arnold Schoenberg, 1978. ISBN 978-0-520-26608-7.
-Chord positions, inversions, harmony basics.
+L<Music::Chord::Note>
 
 =back
 
@@ -183,7 +387,7 @@ Jeremy Mates, E<lt>jmates@cpan.orgE<gt>
 Copyright (C) 2012 by Jeremy Mates
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.14 or,
+it under the same terms as Perl itself, either Perl version 5.16 or,
 at your option, any later version of Perl 5 you may have available.
 
 =cut
--- t/Music-Tension-Cope.t
+++ t/Music-Tension-Cope.t
@@ -1,26 +1,83 @@
 use strict;
 use warnings;
 
-use Test::More tests => 8;
+use Test::More tests => 21;
 BEGIN { use_ok('Music::Tension::Cope') }
 
 my $mt = Music::Tension::Cope->new;
 
-is( $mt->pitches( 0, 0 ),  0, 'unison tension test' );
-is( $mt->pitches( 0, 12 ), 0, 'octave tension test' );
+# Worked out from p.233 [Cope 2005] example, XXX find his lookup tables
+# in published code? If so, could have { metername => { beatnum => value, ...
+# lookup hash included in this module.
+is( $mt->metric( 1, 2 ), 0.05, 'metric 4/4 beat 1' );
+is( $mt->metric( 2, 2 ), 0.1,  'metric 4/4 beat 2' );
+is( $mt->metric( 3, 6 ), 0.05, 'metric 4/4 beat 2' );
+is( $mt->metric( 4, 2 ), 0.2,  'metric 4/4 beat 2' );
 
-is( $mt->pitches( 0, 1 ),  1.0,  'minor 2nd tension test' );
-is( $mt->pitches( 0, 13 ), 0.98, 'minor 2nd +8va tension test' );
+is( $mt->pitches( 0, 0 ),  0, 'unison tension' );
+is( $mt->pitches( 0, 12 ), 0, 'octave tension' );
 
+is( $mt->pitches( 0, 1 ),  1.0,  'minor 2nd tension' );
+is( $mt->pitches( 0, 13 ), 0.98, 'minor 2nd +8va tension' );
+# multiple octaves no more consonant
+is( $mt->pitches( 0, 25 ), 0.98, 'minor 2nd +8va*2 tension' );
+
+# approach mostly just calls ->pitches(0, x)
+is( $mt->approach(0), 0,   'unison approach tension' );
+is( $mt->approach(7), 0.1, 'fifth approach tension' );
+
+# vertical depends on ->pitches working
 is_deeply(
-  [ $mt->pcs( [qw/0 3 7/] ) ],
-  [ 0.175, 0.1, 0.225, [ 0.225, 0.1, 0.2 ] ],
-  'pcs test'
+  [ $mt->vertical( [qw/0 3 7/] ) ],
+  [ 0.325, 0.1, 0.225, [ 0.225, 0.1 ] ],
+  'vertical tension'
 );
 
+# repositioning edge cases
+is_deeply(
+  [ $mt->vertical( [qw/14 1 2 3 12 13/] ) ],
+  [ 3.5, 0, 1, [ 0.9, 0, 1, 0.7, 0.9 ] ],
+  'vertical reposition, single register'
+);
+is_deeply(
+  [ $mt->vertical( [qw/60 11 12 13/] ) ],
+  [ 1.9, 0, 1, [ 0.9, 0, 1 ] ],
+  'vertical reposition, multiple registers'
+);
+
+is( $mt->duration( 0.3, 0.25 ),
+  0.055, 'major triad qn duration given tension' );
+
+# duration-supplied-pitch_set depends on ->vertical working
+is( scalar $mt->duration( [qw/0 4 7/], 0.25 ),
+  0.055, 'major triad qn duration lookup tension' );
+
+########################################################################
+#
+# new() params
+
 my $mtc = Music::Tension::Cope->new(
-  tensions      => { 0 => 0.33, 1 => 0.5 },
-  octave_adjust => 0.2,
+  duration_weight => 0.5,
+  metric_weight   => 0.5,
+  octave_adjust   => 0.2,
+  tensions        => {
+    0  => 0.33,
+    1  => 0.5,
+    2  => 0,
+    3  => 0,
+    4  => 0,
+    5  => 0,
+    6  => 0,
+    7  => 0,
+    8  => 0,
+    9  => 0,
+    10 => 0,
+    11 => 0
+  },
 );
 is( $mtc->pitches( 0, 0 ),  0.33, 'unison tension test (custom)' );
 is( $mtc->pitches( 0, 13 ), 0.7,  'minor 2nd +8va tension test (custom)' );
+
+is( $mtc->duration( 0.3, 0.25 ),
+  0.275, 'major triad qn duration custom weight' );
+is( $mtc->metric( 1, 2 ), 0.25, 'metric 4/4 beat 1 custom weight' );
